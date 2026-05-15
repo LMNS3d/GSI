4a123644c94bcdcd61e1e0358755b2c770a20c5c
Lin Fu 傅林 <LMNS3d@users.noreply.github.com>
2026-05-15 17:40:31 +0800

    Update main_TimeSeries.m

--- Case_TBL/main_TimeSeries.m
+++ Case_TBL/main_TimeSeries.m
@@ -1,5 +1,5 @@
 % Reference paper: Ying, Li & Fu, Generalized synthetic inflow generation %
-% method for divergence-free inhomogeneous turbulence, JCP, 2025          %
+% method for divergence-free inhomogeneous turbulence, JCP, 2026          %
 % Authors: Anjia Ying, Zhigang Li, Lin Fu                                 %
 
 %%%%% Example program: Generate temporal series at the inlet boundary of 
